99723d946e3415d5d9d74303c90072ab9f8f28ed
Joseph Jackson <joejak93@gmail.com>
2025-05-02 23:56:49 -0500

    add support for second optional api

--- src/routes/api/genword/+server.ts
+++ src/routes/api/genword/+server.ts
@@ -1,21 +1,23 @@
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-
 const bannedwords: string[] = [];
 
 export const GET: RequestHandler = async () => {
-  const response = await fetch(
-    "https://random-word-api.herokuapp.com/word?number=1000"
+  const response = fetch(
+    "https://random-word-api.herokuapp.com/word?number=100"
   );
-  const body = await response.json();
-  let startList = body as Array<string>;
-  startList = startList.filter((term)=>{
+  const response2 = fetch("https://random-word-api.vercel.app/api?words=100");
+
+  let winner = await Promise.race([response, response2]);
+  const body = await winner.json();
+  let startList = body as unknown as Array<string>;
+  startList = startList.filter((term) => {
     return !bannedwords.includes(term.toLowerCase());
-  })
-  startList = startList.filter((term)=>{
+  });
+  startList = startList.filter((term) => {
     return term.length < 8 && term.length > 3;
-  })
+  });
   let ret = startList[Math.floor(Math.random() * startList.length)];
   return json(ret);
 };
